20d99e810442b249575d32e4b3cd0c0063d38b6a
喜欢、陪你看风景 <programmer_zzq@163.com>
2017-09-03 15:13:35 +0800

    update

---
+++ app/src/main/java/coder/zzq/appstructure/MainActivity.java
@@ -0,0 +1,15 @@
+package coder.zzq.appstructure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
