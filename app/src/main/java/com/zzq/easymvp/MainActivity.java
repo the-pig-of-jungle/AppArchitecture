8539d94c22d813a06e6b02e932f41d01dc28a69e
coder_zzq <code_zzq@163.com>
2018-08-11 19:39:49 +0800

    update

---
+++ app/src/main/java/com/zzq/easymvp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zzq.easymvp;
+
+import android.arch.lifecycle.LiveData;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
