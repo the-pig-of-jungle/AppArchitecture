64647cf7ba3fc75b8bcab388f90305ed6e94f0dc
nannan.wang <nannan.wang@partner.bmw.com>
2019-05-31 22:03:53 +0800

    refactor

--- app/src/main/java/com/zzq/mvpstorm/MainActivity.java
+++ app/src/main/java/com/zzq/mvpstorm/MainActivity.java
@@ -12,22 +12,14 @@ import cn.droidlover.xdroidmvp.mvp.XActivity;
 import cn.droidlover.xdroidmvp.mvp.XFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private SMSObserver smsObserver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        smsObserver = new SMSObserver(new Handler(),this);
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.READ_SMS,
-                Manifest.permission.RECEIVE_SMS},1000);
-        Uri uri = Uri.parse("content://sms/");
-        getContentResolver().registerContentObserver(uri,true,smsObserver);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        getContentResolver().unregisterContentObserver(smsObserver);
     }
 }
