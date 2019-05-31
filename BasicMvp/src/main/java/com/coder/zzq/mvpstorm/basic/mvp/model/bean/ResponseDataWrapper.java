0c4fbdc454dace4f9b34b062d6595d864b05f5f3
zhuzhiqiang <coder_zzq@163.com>
2019-05-31 23:48:40 +0800

    修改子库依赖名字

--- BasicMvp/src/main/java/com/coder/zzq/mvpstorm/basic/mvp/model/bean/ResponseDataWrapper.java
+++
@@ -1,7 +0,0 @@
-package com.coder.zzq.mvpstorm.basic.mvp.model.bean;
-
-public interface ResponseDataWrapper<DATA> {
-    int code();
-    String msg();
-    DATA data();
-}
--- BasicMvp/src/main/java/com/coder/zzq/mvpstorm/basic/mvp/model/biz/BasicMvpBizImpl.java
+++
@@ -1,7 +0,0 @@
-package com.coder.zzq.mvpstorm.basic.mvp.model.biz;
-
-import com.coder.zzq.mvpstorm.basic.mvp.contract.BasicMvpContract;
-
-public class BasicMvpBizImpl implements BasicMvpContract.BasicMvpBiz {
-
-}
