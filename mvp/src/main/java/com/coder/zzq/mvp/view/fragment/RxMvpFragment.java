6a6e615b12904b597feb51efca62dc862691e29c
nannan.wang <nannan.wang@partner.bmw.com>
2019-05-31 22:44:01 +0800

    refactor

--- mvp/src/main/java/com/coder/zzq/mvp/view/fragment/RxMvpFragment.java
+++ mvp/src/main/java/com/coder/zzq/mvp/view/fragment/RxMvpFragment.java
@@ -1,13 +1,12 @@
 package com.coder.zzq.mvp.view.fragment;
 
-import com.coder.zzq.mvpstorm.basic.mvp.view.fragment.BasicMvpFragment;
-import com.coder.zzq.mvpstorm.retrorx.mvp.contract.RxMvpContract;
+import com.coder.zzq.mvp.contract.MvpContract;
 
 import io.reactivex.subjects.BehaviorSubject;
 
 
-public abstract class RxMvpFragment<P extends RxMvpContract.RxMvpPresenter> extends BasicMvpFragment<P>
-        implements RxMvpContract.RxMvpView<P> {
+public abstract class RxMvpFragment<P extends MvpContract.MvpPresenter> extends MvpFragment<P>
+        implements MvpContract.RxMvpView<P> {
     private BehaviorSubject<String> mBehaviorSubject = BehaviorSubject.create();
 
     public BehaviorSubject<String> getBehaviorSubject() {
